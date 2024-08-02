c223e2039dc1673ad348acdd6742a52956b41d4c
grunsh <alexander@grunin.ru>
2024-08-02 23:15:30 +0300

    Чуток почистил

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -5,14 +5,17 @@ import (
 	"golang.org/x/example/hello/reverse"
 )
 
+// Реализуем в виде своей функции, вдруг потом надо будет дополнить фичами
 func retReversString(s string) string {
 	return reverse.String(s)
 }
 
 func main() {
 
+	// Строка для перевёртыша
 	exampleString := "Hello, OTUS!"
 
+	//Выводим в стандартный вывод вывернутую строку
 	fmt.Println(retReversString(exampleString))
 
 }
